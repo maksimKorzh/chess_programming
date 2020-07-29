4b52d6b1882a62287ab5c8da6193104ca30f643e
Maksim Korzh <freesoft.for.people@gmail.com>
2020-07-29 15:34:14 +0300

    added board representation and piece encoding tutorial

---
+++ src/chess_0x88/tutorials/ep_1/chess_0x88.c
@@ -0,0 +1,82 @@
+/************************************************\
+
+               Mailbox Chess Engine
+                      (0x88)
+
+                        by
+
+                 Code Monkey King
+
+\************************************************/
+
+// headers
+#include <stdio.h>
+
+// piece encoding
+enum {e, P, N, B, R, Q, K, p, n, b, r, q, k, o};
+
+// ascii pieces
+char ascii_pieces[] = ".PNBRQKpnbrqk";
+
+// unicode pieces
+char *unicode_pieces[] = {".", "♙", "♘", "♗", "♖", "♕", "♔", "♟︎", "♞", "♝", "♜", "♛", "♚"};
+
+// chess board representation
+int board[128] = {
+    r, n, b, q, k, b, n, r,  o, o, o, o, o, o, o, o,
+    p, p, p, p, p, p, p, p,  o, o, o, o, o, o, o, o,
+    e, e, e, e, e, e, e, e,  o, o, o, o, o, o, o, o,
+    e, e, e, e, e, e, e, e,  o, o, o, o, o, o, o, o,
+    e, e, e, e, e, e, e, e,  o, o, o, o, o, o, o, o,
+    e, e, e, e, e, e, e, e,  o, o, o, o, o, o, o, o,
+    P, P, P, P, P, P, P, P,  o, o, o, o, o, o, o, o,
+    R, N, B, Q, K, B, N, R,  o, o, o, o, o, o, o, o
+};
+
+// print board
+void print_board()
+{
+    // print new line
+    printf("\n");
+
+    // loop over board ranks
+    for (int rank = 0; rank < 8; rank++)
+    {
+        for (int file = 0; file < 16; file++)
+        {
+            // init square
+            int square = rank * 16 + file;
+            
+            // print ranks
+            if (file == 0)
+                printf(" %d  ", 8 - rank);
+            
+            // if square is on board
+            if (!(square & 0x88))
+                //printf("%c ", ascii_pieces[board[square]]);
+                printf("%s ", unicode_pieces[board[square]]);
+        }
+        
+        // print new line every time new rank is encountered
+        printf("\n");
+    }
+    
+    // print files
+    printf("\n    a b c d e f g h\n\n");
+}
+
+// main driver
+int main()
+{
+    print_board();
+    printf("♔");
+    return 0;
+}
+
+
+
+
+
+
+
+
