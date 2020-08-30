56cc7cf509bb6987fb4d8cb8634689356463f5c9
Maksim Korzh <freesoft.for.people@gmail.com>
2020-08-30 16:44:00 +0300

    fixed move formatting constant for promoted piece

--- src/bbc/move_formatting/bbc.c
+++ src/bbc/move_formatting/bbc.c
@@ -1762,7 +1762,7 @@ void init_all()
     0000 0000 0000 0000 0011 1111    source square       0x3f
     0000 0000 0000 1111 1100 0000    target square       0xfc0
     0000 0000 1111 0000 0000 0000    piece               0xf000
-    0000 1111 0000 0000 0000 0000    promoted piece      0xf000
+    0000 1111 0000 0000 0000 0000    promoted piece      0xf0000
     0001 0000 0000 0000 0000 0000    capture flag        0x100000
     0010 0000 0000 0000 0000 0000    double push flag    0x200000
     0100 0000 0000 0000 0000 0000    enpassant flag      0x400000
