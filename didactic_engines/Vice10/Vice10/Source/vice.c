c67b2f0398c464aa7a37aa7bf10e2db4924658ce
Maksim Korzh <freesoft.for.people@gmail.com>
2020-12-20 17:04:04 +0200

    added talkchess reply on tactical vs positional playing style

--- didactic_engines/Vice10/Vice10/Source/vice.c
+++ didactic_engines/Vice10/Vice10/Source/vice.c
@@ -21,6 +21,9 @@ int main() {
 	setbuf(stdin, NULL);
     setbuf(stdout, NULL);
 	
+	ParseFen("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1 ", pos);
+	PerftTest(4, pos);
+	
 	printf("Welcome to Vice! Type 'vice' for console mode...\n");
 
 	char line[256];
